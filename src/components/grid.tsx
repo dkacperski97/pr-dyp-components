684d01e86fa380b58f77d0479db640c221d996a7
Damian Kacperski <7dami77@gmail.com>
2021-03-29 01:10:02 +0000

    Grid component

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import Component from '../types/component';
+import Props from '../types/props';
+import DropTarget from './helpers/DropTarget';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      display: 'flex',
+    },
+});
+
+const Grid: React.FC<Props> = ({ children, setChild }) => {
+    const classes = useStyles();
+	const amount = 3;
+	const getContent = () => {
+		const content = [];
+		for (let a = 0; a < amount; a++) {
+			content.push(children[a] !== undefined ? children[a] : (<DropTarget key={a} index={a} setChild={setChild} />));
+		}
+		return content;
+	}
+	return (
+		<div className={classes.root}>
+			{getContent()}
+		</div>
+	);
+};
 
 const grid: Component = {
 	id: 'grid',
 	options: [],
-	component: () => {
-		return <div></div>;
-	},
+	component: Grid,
 	template: '',
 };
 
