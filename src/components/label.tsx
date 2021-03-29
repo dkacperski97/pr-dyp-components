d99f94b00c0ad57e3d19fbe404651a9c30be0bcf
Damian Kacperski <7dami77@gmail.com>
2021-03-29 01:21:13 +0000

    Label component

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Component from '../types/component';
+import InputLabel from '@material-ui/core/InputLabel';
+import Props from '../types/props';
+
+const Label: React.FC<Props> = () => {
+	return (
+        <InputLabel>Name</InputLabel>
+	);
+};
 
 const label: Component = {
 	id: 'label',
 	options: [],
-	component: () => {
-		return <div></div>;
-	},
+	component: Label,
 	template: '',
 };
 
