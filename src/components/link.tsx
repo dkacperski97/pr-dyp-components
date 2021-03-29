a958e20909b4d1949fc7aae3fe60762b80ee088f
Damian Kacperski <7dami77@gmail.com>
2021-03-29 01:08:22 +0000

    Link component

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Component from '../types/component';
+import Button from '@material-ui/core/Button';
+import Props from '../types/props';
 
+const Link: React.FC<Props> = () => {
+	return (
+		<Button variant="contained">Text</Button>
+	)
+}
 const link: Component = {
 	id: 'link',
 	options: [],
-	component: () => {
-		return <div></div>;
-	},
+	component: Link,
 	template: '',
 };
 
