8c25b200ce5ae3751ead33094ebdecd8fc413539
Damian Kacperski <7dami77@gmail.com>
2021-03-29 01:28:40 +0000

    List component

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import Component from '../types/component';
+import Props from '../types/props';
+import MuiList from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const List: React.FC<Props> = () => {
+	const values = ["aaaaa", "bbbbb", "ccccc"];
+	return (
+		<MuiList>
+			{values.map(v =>
+				<ListItem button key={v}>
+					<ListItemText
+						primary={v}
+					/>
+				</ListItem>
+			)}
+		</MuiList>
+	);
+};
 
 const list: Component = {
 	id: 'list',
 	options: [],
-	component: () => {
-		return <div></div>;
-	},
+	component: List,
 	template: '',
 };
 
