194ce644cc6e48bb1e844ecd17f0a085bcb27d3e
Damian Kacperski <7dami77@gmail.com>
2021-04-06 11:28:08 +0000

    Replaced tsdx with preconstruct

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export type { default as Component } from './types/component';
+export type { default as Props } from './types/props';
+export { default } from './components';
--- src/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Component } from './types/component';
-export { default as Props } from './types/props';
-export { default } from './components';
